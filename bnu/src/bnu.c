a45065a21985f00fc3414f1cb0e4fcb9001006b2
wust.ajax <wust.ajax@gmail.com>
2013-06-04 07:50:45 +0000

    bun 代码仓库

---
+++ bnu/src/bnu.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : bnu.c
+ Author      : IORI
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
