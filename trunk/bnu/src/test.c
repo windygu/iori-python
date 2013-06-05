fb6388a1c8e66c372c89c5dcdf4552114d8b2a0c
wust.ajax@gmail.com <wust.ajax@gmail.com@384602d9-a251-11e7-a0fd-42cf4459d645>
2013-06-05 09:01:12 +0000

    测试用样子

---
+++ trunk/bnu/src/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+
+
+
+static void solve(void){
+	printf("the first change");
+	printf("second change comes here");
+	printf("the third time changes");
+	
+}
+int main(void){
+	freopen("data.dat", "r", stdin);
+	solve();
+	return 0;
+}
